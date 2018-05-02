160eff146d9ae5ab97860011df9b67683b87c235
Chethan <crajendra@qwinix.io>
2018-05-02 13:04:23 +0530

    added lines

--- features/events.feature
+++ features/events.feature
@@ -1,39 +1,17 @@
-@events
-Feature: As a login user
-I should able to open a event form
+@login
+Feature: As a end user
+I should able to login to the application
+So  that I can create event schedular
 
-@login_page
-Scenario:
-Given I am on event page
-When I click on login button it should open a form
-Then I will see the form
+@smoke
+Scenario:trying to login with valid credentials
+Given I am on home page 
+When I login with valid credentials 
+Then I should see "Logged in!"
 
-@insert_fields
-Scenario:
-Given I am in login form
-When I have given the valid email and password and click OK
-Then I should be able to open home page
+@creating_an_events
+Scenario:trying to creating an events with valid credentials
+Given I am in event schedular dashboard
+When I create the new event with valid data
+Then I should see the events created in the list
 
-@home_page
-Scenario:
-Given I am in home page
-When I click on Create New Event
-Then I should see Create New Event form
-
-@fill_all_fields
-Scenario:
-Given I am in Create New Events form
-When I fill all the fields and click on OK button
-Then I should get sucessfully login message
-
-@check_event
-Scenario:
-Given I am in home page
-When I click on 2 it goes to next list
-Then I should get the name form who are in the list
-
-@check_events
-Scenario:
-Given I am in home page
-When I click on 3 it goes to next list
-Then I should get the name form who are in the list
--- features/step_definitions/event_steps.rb
+++ features/step_definitions/event_steps.rb
@@ -1,83 +1,34 @@
 
-Given("I am on event page") do
+Given("I am on home page") do
 	@browser.navigate.to  "http://eventscheduling.herokuapp.com/"
-  # pending # Write code here that turns the phrase above into concrete actions
 end
 
-When("I click on login button it should open a form") do
+When("I login with valid credentials") do
 	@browser.find_element(xpath: '//div/div[2]/ul/li[2]/form/input').click
 	sleep 2
-
-  # pending # Write code here that turns the phrase above into concrete actions
-end
-
-Then("I will see the form") do
-	@browser.find_element(xpath: ".//*[@id='myModal']/div/div").displayed?
-	sleep 2
-  # pending # Write code here that turns the phrase above into concrete actions
-end
-
-Given("I am in login form") do
-	@browser.find_element(xpath: ".//*[@id='myModal']/div/div").displayed?
-	sleep 2
-  # pending # Write code here that turns the phrase above into concrete actions
-end
-
-When("I have given the valid email and password and click OK") do
 	@browser.find_element(name: 'email').send_keys "pajju75@gmail.com"
 	sleep 2
 	@browser.find_element(name: 'password').send_keys "uthsavgowda"
 	sleep 2
-
-
-  # pending # Write code here that turns the phrase above into concrete actions
-end
-
-Then("I should be able to open home page") do
 	@browser.find_element(name: 'commit').click
 	sleep 2
-  # pending # Write code here that turns the phrase above into concrete actions
 end
 
-# Given("I am in Create New Event") do
-# 	@browser.find_element(xpath: '//body/div[3]/div/div/div[3]').displayed?
-#   # pending # Write code here that turns the phrase above into concrete actions
-# end
-
-# When("I click on Create New Event") do
-# 	@browser.find_element(xpath: '//body/div[3]/div/div/div[1]/form/input').click
-#   # pending # Write code here that turns the phrase above into concrete actions
-# end
-
-# Then("I should see Create New Event form") do
-# 	@browser.find_element(xpath: './/*[@id='popup_modal']/div[2]/div/div[1]').displayed?
-#   # pending # Write code here that turns the phrase above into concrete actions
-# end
-
-Given("I am in home page") do
-	@browser.find_element(xpath: '//div[3]/div/div/div[3]')
-  # pending # Write code here that turns the phrase above into concrete actions
+Then("I should see {string}") do |string|
+	@browser.find_element(xpath: ".//*[@id='div_flash_container']").displayed?
 end
 
-When("I click on Create New Event") do
+
+Given("I am in event schedular dashboard") do
+	@browser.find_element(xpath: '//div/div[2]/div').displayed?
 	sleep 2
-	@browser.find_element(xpath: '//div[3]/div/div/div[1]/form/input').click
-  # pending # Write code here that turns the phrase above into concrete actions
-end
-
-Then("I should see Create New Event form") do
-	sleep 3
-	@browser.find_element(id:'popup_modal').displayed?
-  # pending # Write code here that turns the phrase above into concrete actions
-end
-
-Given("I am in Create New Events form") do
-	@browser.find_element(id:'popup_modal')
+	@browser.find_element(xpath: '//div/div/div[1]/form/input').click
 	sleep 2
 end
 
-When("I fill all the fields and click on OK button") do
-	@browser.find_element(id: 'event_name').send_keys "games"
+When("I create the new event with valid data") do
+	sleep 2
+	@browser.find_element(id: 'event_name').send_keys "cricket"
 	sleep 2
 	@browser.find_element(name: 'event[venue]').send_keys "mysore"
 	sleep 2
@@ -85,27 +36,35 @@ When("I fill all the fields and click on OK button") do
 	sleep 2
 	@browser.find_element(id: 'event_time').send_keys "10:15"
 	sleep 2
-	@browser.find_element(name: 'event[description]').send_keys "hello"
+	@browser.find_element(name: 'event[description]').send_keys "hello goodmorning"
 	sleep 2
 	@browser.find_element(id: 'signup_button').click
 	sleep 2
-
-
 end
 
-Then("I should get sucessfully login message") do
- @browser.find_element(id: 'event_form').displayed?
+Then("I should see the events created in the list") do
+	sleep 2
+	var = @browser.find_element(xpath: '//div[3]/div/div/div[4]/div[1]').text
+	var1 = "CRICKET"
+	if var.include? var1
+		puts "#{var1} Event created succussfully!"
+	else
+		raise puts "#{var1} Not found"
+	end
 end
 
-When("I click on {int} it goes to next list") do |int|
-@browser.find_element(xpath: 'html/body/div[3]/div/div/div[4]/div[1]/nav/span[2]/a').click
-sleep 2
-end
 
-Then("I should get the name form who are in the list") do
-@browser.find_element(xpath: 'html/body/div[3]/div/div/div[4]/div[1]/div[1]/div').displayed?
-sleep 2
-end
+
+
+
+
+
+
+
+
+
+
+
 
 
 
